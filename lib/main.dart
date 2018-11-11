ed9fac3e49554eca618c2de0305e4a7ff3d9ca50
luis_mantilla <lmmantilla9350@gmail.com>
2018-11-10 23:36:13 -0400

    Add libraries

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     Center(
-      child: Text("Hello DevFest Cochabamba!!", textDirection: TextDirection.ltr,),
+      child:
+        Text(
+          "Hello DevFest Cochabamba!!",
+          textDirection: TextDirection.ltr,
+        ),
     )
   );
 }
